3675455dc8092397722a3759d67e497f4bb156dc
Alberto <alberto@ubuntu.(none)>
2014-12-16 20:42:42 +0100

    proxy+invite

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -10,11 +10,13 @@ import os
 import client
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-
+import time
 class SIPHandler(SocketServer.DatagramRequestHandler):
     """
     SIP server class
     """
+    diccionario{}
+
     def procesar(self, line, HOST):
         """
         Procesa los mensajes recibidos
@@ -22,20 +24,70 @@ class SIPHandler(SocketServer.DatagramRequestHandler):
         # Extraemos el metodo que nos envia el cliente
         linea = line.split(" ")
         metodo = linea[0]
-        IP = str(self.client_address[0])
-        if metodo == "INVITE":
-            #Obtenemos la informacion contenida en SDP
-            informacion=line.split('\r\n')
-            IP_CLIENT = informacion[4].split(" ")[1]
-            PORT_CLIENT = informacion[7].split(" ")[1]
-            #Creamos los mensajes de respuesta
-            line = 'SIP/2.0 100 Trying' + '\r\n' + '\r\n'
-            line += 'SIP/2.0 180 Ringing' + '\r\n' + 'Content-Type: '
-            line += 'SIP/2.0 200 OK' + '\r\n' + '\r\n' 
-            line += 'apliccation/SDP\r\n\r\nv=0\r\no=' + registro['username']
-            line += registro['uaserver_ip'] + '\r\ns=misesion\r\nt=0m=audio'
-            line += registro['rtpaudio_puerto'] 
-            self.wfile.write(line)
+        direccion = linea[1].split(":")[1]
+        if metodo == "REGISTER":
+            #Obtenemos los datos del cliente y los guardamos en un diccionario
+            IP_CLIENT = str(self.client_address[0])
+            PORT_CLIENT = linea[1].split(":")[-1]
+            expires = linea[-1].split(":")[-1]
+            EXPIRES = expires.split("\r\n")[0]
+            lista = (IP_CLIENT,PORT_CLIENT)
+            self.diccionario[direccion] = lista
+            fecha_registro = time.time()
+            #Abrimos el fichero para escribir los datos del nuevo usuario
+            database = open(registro["database_path"], a)
+            database.write(direccion + "\t" + IP_CLIENT + "\t" + PORT_CLIENT
+            + "\t" + fecha_registro + EXPIRES + '\r\n')
+            #Abrimos el fichero log y escribimos el mensaje recibido
+            log_proxy=open(regitro["log_path"], a)
+            hora = time.strftime('%Y%m%d%H%M%S',
+            time.gmtime(time.time()))
+            log_proxy.write(hora + " Received from " + IP_CLIENT + ":"
+            + PORT_CLIENT + ":" + line + '\r\n')
+        else
+            #Buscamos al usuario en el diccionario
+            reg_direcciones = self.diccionario.keys()
+            registrado = false
+            n = 0
+            while not registrado or n < len(reg_direcciones):
+                if direccion = reg_direcciones(n):
+                    encontrado = true
+                    direccion = reg_direcciones(n)
+                else: 
+                    n = n + 1
+            if registrado:
+                if metodo == "INVITE":
+                    #Escribimos el mensaje recibido
+                    hora = time.strftime('%Y%m%d%H%M%S',
+                    time.gmtime(time.time()))
+                    log_proxy.write(hora + " Received from " + ip_ua1 + ":"
+                    + port_ua1 + ":" + line + '\r\n')
+                    #Obtenemos los datos del cliente y el destinatario
+                    informacion=line.split('\r\n')
+                    ip_ua1 = informacion[4].split(" ")[1]
+                    port_ua1 = informacion[7].split(" ")[1]
+                    ip_ua2 = self.diccionario[direccion][0]
+                    port_ua2 = self.diccionario[direccion][1] 
+                    #Enviamos el mensaje a su destino y esperamos respuesta
+                    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                    my_socket.connect((ip_ua2, int(port_ua2)))
+                    my_socket.send(line)
+                    hora = time.strftime('%Y%m%d%H%M%S',
+                    time.gmtime(time.time()))
+                    log_proxy.write(hora + "Send to " + ip_ua2 + ":" + port_ua2
+                    + ":" + line + '\r\n')
+                    data = my_socket.recv(1024)
+                    hora = time.strftime('%Y%m%d%H%M%S',
+                    time.gmtime(time.time()))
+                    log_proxy.write(hora + ' Received from ' + ip_ua2 + ":"
+                    + port_ua2 + data + '\r\n')
+                     #Enviamos la contestacion del destinatario al cliente
+                    self.wfile.write(data)
+                    print data
+                    log_proxy.write(hora + "Send to " + ip_ua1 + ":" + port_ua1
+                    + ":" + data + '\r\n')
+                    
         elif metodo == "ACK":
             #Enviamos el audio
             AUDIO = registro['audio_path']
@@ -79,4 +131,3 @@ if __name__ == "__main__":
     serv = SocketServer.UDPServer((HOST, PORT), SIPHandler)
     print "Server " + registro["name"] + " Listening at port " + registro["port"] 
     serv.serve_forever()
-
