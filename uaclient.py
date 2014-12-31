6670546c46f530ebeedce996552d9ef221d2e693
Alberto <alberto@ubuntu.(none)>
2014-12-31 20:59:40 +0100

    actualizaciones

--- uaclient.py
+++ uaclient.py
@@ -10,30 +10,33 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import time
 import uaserver
+import proxy_registrar
 # Cliente SIP simple.
 
-def procesar_contestacion(data):
-    #analiza la contestacion
-    hora = time.strftime('%Y%m%d%H%M%S',
-    time.gmtime(time.time()))
-    log_ua.write(hora + "Received from " + ip_proxy
-    + ":" + puerto_proxy + data + '\r\n')
-    contestacion = data.split(" ")
-    if len(contestacion) >= 5 and contestacion[5] == "200":
-        #obtenemos datos de sdp para enviar audio
-        ip_rtp = contestacion[7].split("\r\n")[0]
-        puerto_rtp = contestacion[8]
-        line = "ACK " + 'sip:' + opcion + ' SIP/2.0'
-        line += '\r\n' + '\r\n'
-        my_socket.send(line)
-        hora = time.strftime('%Y%m%d%H%M%S',
-        time.gmtime(time.time()))
-        log_ua.write(hora + "Send to " + ip_proxy
-        + ":" + puerto_proxy + line + '\r\n')
-        AUDIO = registro['audio_path']
-        aEjecutar = './mp32rtp -i ' + ip_rtp + ' -p ' + puerto_rtp + ' < ' + AUDIO
-        os.system(aEjecutar)
-        print "Audio enviado"
+
+if __name__ == "__main__":
+
+    def procesar_contestacion(data):
+        #analiza la contestacion
+        linea = "Received from " + ip_proxy
+        linea += ":" + puerto_proxy + data
+        log.wr(log_path, linea)
+        contestacion = data.split(" ")
+        if len(contestacion) >= 5 and contestacion[5] == "200":
+            #obtenemos datos de sdp para enviar audio
+            ip_rtp = contestacion[7].split("\r\n")[0]
+            port_rtp = contestacion[8]
+            line = "ACK " + 'sip:' + opcion + ' SIP/2.0'
+            line += '\r\n' + '\r\n'
+            my_socket.send(line)
+            my_socket.recv(1024)      
+            linea = "Send to " + ip_proxy
+            linea += ":" + puerto_proxy + " " + line
+            log.wr(log_path, linea)
+            #enviamos el audio y lanzamos el hilo de escucha
+            audio = registro["audio_path"]
+            reproducir = uaserver.send_audio(ip_rtp, port_rtp, audio, ip_ua, puerto_rtp)
+            reproducir.enviar()
 try:
     CONFIG = sys.argv[1]
     metodo = sys.argv[2].upper()
@@ -43,6 +46,7 @@ try:
     parser.setContentHandler(cHandler)
     parser.parse(open(CONFIG))
     registro = cHandler.diccionario
+    log = proxy_registrar.write_log()
     #Obtenemos los datos contenidos en DTD
     usuario = registro["username"]
     ip_ua = registro["uaserver_ip"]
@@ -50,10 +54,11 @@ try:
     ip_proxy = registro["regproxy_ip"]
     puerto_proxy = registro["regproxy_puerto"]
     puerto_rtp = registro["rtpaudio_puerto"]
-    log = registro["log_path"]
+    print puerto_rtp
+    log_path = registro["log_path"]
     hora = time.strftime('%Y%m%d%H%M%S',
     time.gmtime(time.time()))
-    log_ua = open(log, "a")
+    log_ua = open(log_path, "a")
     log_ua.write("...\r\n")
     #Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -63,20 +68,20 @@ try:
         line = "REGISTER " + "sip:" + usuario + ":" + puerto_ua
         line += " SIP/2.0" + "\r\n" + "Expires:" + opcion + "\r\n" + "\r\n"
         log_ua.write(hora + " Starting..." + "\r\n")
-        hora = time.strftime('%Y%m%d%H%M%S',
-        time.gmtime(time.time()))
+        log_ua.close()
         evento = " Sent to " + ip_proxy + ":" + puerto_proxy + ":" + line
-        log_ua.write(hora + evento + "\r\n")
+        log.wr(log_path, evento)
         my_socket.send(line)
+        data = my_socket.recv(1024)
+        print data
     elif metodo == "INVITE":
         line = "INVITE " + "sip:" + sys.argv[3]
         line += " SIP/2.0" + "\r\n" + "Content-Type: application/sdp\r\n\r\n"
         line += "v=0\r\no=" + usuario + " " + ip_ua + "\r\ns=misesion"
         line += "\r\nt=0\r\nm=audio " + puerto_rtp + " RTP"
-        hora = hora = time.strftime('%Y%m%d%H%M%S',
-        time.gmtime(time.time()))
-        evento = " Sent to " + ip_proxy + ":" + puerto_proxy + line
-        log_ua.write(hora + evento + "\r\n")
+        evento = " Sent to " + ip_proxy + ":" + puerto_proxy
+        evento += " " +  line
+        log.wr(log_path, evento)
         #Recibimos la contestacion y la anlizamos
         my_socket.send(line)
         data = my_socket.recv(1024)
@@ -84,16 +89,15 @@ try:
         procesar_contestacion(data)
     elif metodo == "BYE":
         line = "BYE " + "sip:" + sys.argv[3] + " SIP/2.0\r\n\r\n"
-        hora = time.strftime('%Y%m%d%H%M%S',
-        time.gmtime(time.time()))
-        evento = " Sent to " + ip_proxy + ":" + puerto_proxy + line
-        log_ua.write(hora + evento + "\r\n")
+        evento = " Sent to " + ip_proxy + ":" + puerto_proxy
+        evento += " " + line
+        log.wr(log_path, evento)
         my_socket.send(line)
         data = my_socket.recv(1024)
         print data
-    else:    
-        #Si el cliente escribe mal un metodo tambien lo enviamos al servidor se encarga de mandarnos el mensaje de error
-        line = metodo + "sip:" + sys.argv[3] + "SIP/2.0\r\n\r\n"        
+    else:
+        #Sí se escribe cualquier otro metodo lo enviamos
+        line = metodo + "sip:" + sys.argv[3] + "SIP/2.0\r\n\r\n"
         my_socket.send(line)
         data = my_socket.recv(1024)
         print data
@@ -101,14 +105,14 @@ try:
     my_socket.close()
 
 except socket.error:
-    print "Error: No server listening at" + " " + ip_proxy + " " + str(puerto_proxy)
+    print "Error: No server listening at" + " " + ip_proxy + " " + puerto_proxy
     hora = time.strftime('%Y%m%d%H%M%S',
     time.gmtime(time.time()))
-    log_ua.write(hora + "Error: No server listening at" + " " + ip_proxy 
+    log_ua.write(hora + "Error: No server listening at" + " " + ip_proxy
     + " " + str(puerto_proxy))
-#except ValueError:
-#    print "Usage: python uaclient.py config method option"
-#except IndexError:
-#    print "Usage: python uaclient.py config method option"
+except ValueError:
+    print "Usage: python uaclient.py config method option"
+except IndexError:
+    print "Usage: python uaclient.py config method option"
 #except NameError:
-#    print "Usage: python uaclient.py config method option"
+ #   print "Usage: python uaclient.py config method option"
--- uaserver.py
+++ uaserver.py
@@ -9,6 +9,36 @@ import sys
 import os
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+from threading import Thread
+
+class VLC(Thread):
+
+    def __init__(self, ip_ua, puerto_rtp):
+      Thread.__init__(self)
+      self.ip_ua = ip_ua
+      self.puerto_rtp = puerto_rtp
+
+    def run(self):
+        aEjecutar = "cvlc rtp://@" + self.ip_ua + ":" + str(self.puerto_rtp) + " &"
+        print aEjecutar
+        os.system(aEjecutar)
+
+class send_audio():
+
+    def __init__(self, ip_ua, puerto_rtp, audio, send_ip, send_port):
+      self.ip_rtp = ip_ua
+      self.port_rtp = puerto_rtp
+      self.audio = audio
+      self.send_port = send_port
+      self.send_ip = send_ip
+    def enviar(self): 
+        hilo_reproducir = VLC(self.send_ip, self.send_port)
+        hilo_reproducir.start()  
+        aEjecutar = './mp32rtp -i ' + self.ip_rtp + ' -p ' + self.port_rtp
+        aEjecutar += ' < ' + self.audio
+        print aEjecutar
+        os.system(aEjecutar)
+        print "Audio enviado"
 
 class DtdXMLHandler(ContentHandler):
     """
@@ -30,9 +60,9 @@ class DtdXMLHandler(ContentHandler):
             self.guardar(attrs)
         elif name == "audio":
             self.guardar(attrs)
-        elif name =="server":
+        elif name == "server":
             self.guardar(attrs)
-        elif name =="database":
+        elif name == "database":
             self.guardar(attrs)
 
     def guardar(self, attrs):
@@ -40,7 +70,8 @@ class DtdXMLHandler(ContentHandler):
         atributos = attrs.keys()
         n = 0
         while n < len(atributos):
-            self.diccionario[str(atributos[n])] = str(attrs.get(atributos[n], ""))
+            self.diccionario[str(atributos[n])] = str(attrs.get
+            (atributos[n], ""))
             n = n + 1
 
 
@@ -48,7 +79,7 @@ class SIPHandler(SocketServer.DatagramRequestHandler):
     """
     SIP server class
     """
-    lista = [0,1]
+    lista = [0, 1]
 
     def procesar(self, line, HOST):
         """
@@ -60,28 +91,30 @@ class SIPHandler(SocketServer.DatagramRequestHandler):
         IP = str(self.client_address[0])
         if metodo == "INVITE":
             #Obtenemos la informacion contenida en SDP
-            informacion=line.split('\r\n')
+            informacion = line.split('\r\n')
             self.lista[0] = informacion[4].split(" ")[1]
             self.lista[1] = informacion[7].split(" ")[1]
             #Creamos los mensajes de respuesta
             line = 'SIP/2.0 100 Trying' + '\r\n' + '\r\n'
             line += 'SIP/2.0 180 Ringing' + '\r\n' + '\r\n'
             line += 'SIP/2.0 200 OK' + '\r\n'
-            line += 'Content-Type:apliccation/SDP\r\n\r\nv=0\r\no=' + registro['username']
+            line += 'Content-Type:apliccation/SDP\r\n\r\nv=0\r\no='
             line += " " + registro['uaserver_ip'] + '\r\ns=misesion\r\nt=0\r\n'
-            line += 'm=audio ' + registro['rtpaudio_puerto'] + ' RTP\r\n'
+            line += registro['username'] + 'm=audio '
+            line += registro['rtpaudio_puerto'] + ' RTP\r\n'
             self.wfile.write(line)
         elif metodo == "ACK":
             #Enviamos el audio
             AUDIO = registro['audio_path']
-            aEjecutar = './mp32rtp -i ' + self.lista[0] + ' -p ' + self.lista[1] + ' < ' + AUDIO
-            os.system(aEjecutar)
-            print "audio enviado"
+            listen_port = registro['rtpaudio_puerto']
+            reproducir = send_audio(self.lista[0], self.lista[1], AUDIO, HOST, listen_port )
+            reproducir.enviar()
         elif metodo == "BYE":
             line = 'SIP/2.0 200 OK' + '\r\n'
-            line += 'Content-Type:apliccation/SDP\r\n\r\nv=0\r\no=' + registro['username']
-            line += " " + registro['uaserver_ip'] + '\r\ns=misesion\r\nt=0\r\n'
-            line += 'm=audio ' + registro['rtpaudio_puerto'] + ' RTP\r\n' 
+            line += 'Content-Type:apliccation/SDP\r\n\r\nv=0\r\no='
+            line += registro['username'] + " " + registro['uaserver_ip']
+            line += '\r\ns=misesion\r\nt=0\r\n' + 'm=audio '
+            line += registro['rtpaudio_puerto'] + ' RTP\r\n'
             self.wfile.write(line)
         elif metodo != "INVITE" or "ACK" or "BYE":
             line = "SIP/2.0 405 Method Not Allowed" + '\r\n' + '\r\n'
@@ -104,18 +137,19 @@ class SIPHandler(SocketServer.DatagramRequestHandler):
 
 
 if __name__ == "__main__":
-try:
-    CONFIG= sys.argv[1]
-    #Leemos la DTD usando la clase definida en client.py
-    parser = make_parser()
-    sHandler = DtdXMLHandler()
-    parser.setContentHandler(sHandler)
-    parser.parse(open(CONFIG))
-    registro = sHandler.diccionario
-    HOST = registro["uaserver_ip"]
-    PORT = int(registro["uaserver_puerto"])
-    serv = SocketServer.UDPServer((HOST, PORT), SIPHandler)
-    print "Listening..."
-    serv.serve_forever()
-except IndexError:
-    print "Usage: python uaserver.py config"
+
+    try:
+        CONFIG = sys.argv[1]
+        #Leemos la DTD usando la clase definida en client.py
+        parser = make_parser()
+        sHandler = DtdXMLHandler()
+        parser.setContentHandler(sHandler)
+        parser.parse(open(CONFIG))
+        registro = sHandler.diccionario
+        HOST = registro["uaserver_ip"]
+        PORT = int(registro["uaserver_puerto"])
+        serv = SocketServer.UDPServer((HOST, PORT), SIPHandler)
+        print "Listening..."
+        serv.serve_forever()
+    except IndexError:
+        print "Usage: python uaserver.py config"
